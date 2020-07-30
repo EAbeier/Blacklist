b9cc545ecbccef71b68fe0be1ea263e8c38a8aed
EA_Beier <39953649+EAbeier@users.noreply.github.com>
2020-07-30 08:14:03 -0300

    Update db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "127.0.0.1";
-$username = "root";
-$password = "131519";
-$dbName = "bd_blacklist";
+$username = "<user do db>";
+$password = "<senha do db>";
+$dbName = "<nome do db>";
 
 $connect = mysqli_connect($servername, $username, $password, $dbName, 3308);
 mysqli_set_charset($connect,"utf8");
